2a586ffda72e0c6577b4f0dd706eaa44e1f995c8
Sparshlohana <sparsh.lohana@gmail.com>
2022-12-28 15:09:52 +0530

    howOrderWorks

---
+++ notes/Orders/howAnOrderWorks.js
@@ -0,0 +1,43 @@
+// Orders
+
+// UNDERSTANDING HOW ORDER WORKS IN SHOPIFY is an important part of running your business.
+// After a customer places an order, it appears in the ORDER area of shopify.You can also CREATE ORDERS MANUALLY in your shopify admin to record orders that you have made outside of shopify or to send your customers invoices.
+
+// Processing Orders
+// When a customer buys something from your online store or any other sales channel, it appears as a new order in the ORDER page of shopify.
+
+// What happens when you receive an online order
+// When you receive an online order:
+// => You will receive a NEW ORDER NOTIFICATION by email.
+// => The ORDER's page in shopify will show the new order.
+// => The customer receives the order confirmation mail.
+// => Order process begins.
+
+// NOTE: An abandoned checkout is created if the order was not completed, but the customer reached checkout.
+
+// What happens when you process a Shopify POS (point of sale) order.
+// When you make a sale in person by using Shopify POS:
+// => The orders page of shopify shows the order with a payment status of paid or partially paid.
+// => If the customer provided their email address, they will receive a POS receipt by email.
+
+// Typical processing flow for online
+// Every online order goes through 3 stages. At each stage different actions are required in shopify. You can set up Shopify to perform these ACTIONS AUTOMATICALLY, or you can do them manually.
+// 1. If the Order has been placed. We need to CAPTURE PAYMENT.
+
+// CAPTURING PAYMENT:
+// => By default, credit card payments that are made using some additional payment methods are captured automatically. But if your store has set up to capture payments manually, then you have an extra task to do to ensure that you are paid.
+
+// CAPTURE PAYMENTS MANUALLY:
+// => If your store is configured to capture payments manually, then make sure that you get paid by capturing every payment that has the AUTHORIZED status as soon as possible.
+// => The credit card authorization period for capturing payments varies depending on your payment provider. Eg. Shopify payments provide authorization of 7 days for credit card payments. Capture the payment within the AUTHORIZATION PERIOD. After the authorization period ends, you might not be able to capture the payment and collect any money for  the order.
+
+// 2. If the order has been paid, we need to FULFILL THE ORDER
+
+// FULFILLMENT
+// => After your customer places and order and pays for the order, you need to prepare and deliver the items in it. This process is known as order fulfillment.
+// => The fulfillment process usually includes the following:
+// 1. Picking and packaging products in an envelope or a box.
+// 2. Labelling the order for shipment.
+// 3. Shipping the package with a mail carrier.
+
+// 3. If the Order has been fulfilled and paid, Then we need to ARCHIVE THE ORDER
